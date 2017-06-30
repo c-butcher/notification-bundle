5f4118b1801d21126705d59bd5f56805185406db
Chris Butcher <c.butcher@hotmail.com>
2017-06-30 14:58:09 -0700

    Added configuration parsing.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace KungFu\NotificationBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+use KungFu\NotificationBundle\Entity\NotificationSetting;
+use KungFu\NotificationBundle\Service\NotificationSettingFactory;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('notification');
+
+        $rootNode
+            ->children()
+                ->arrayNode('mailer')->isRequired()
+                    ->children()
+                        ->arrayNode('from')
+                            ->children()
+                                ->scalarNode('name')->isRequired()->end()
+                                ->scalarNode('address')->isRequired()->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('reply_to')
+                            ->children()
+                                ->scalarNode('name')->isRequired()->end()
+                                ->scalarNode('address')->isRequired()->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('user')->isRequired()
+                    ->children()
+                        ->scalarNode('class')->isRequired()->end()
+                        ->arrayNode('properties')
+                            ->children()
+                                ->scalarNode('identifier')->defaultValue('id')->end()
+                                ->scalarNode('email')->defaultValue('email')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('settings')
+                    ->children()
+                        ->scalarNode('class')->defaultValue(NotificationSetting::class)->end()
+                        ->scalarNode('factory')->defaultValue(NotificationSettingFactory::class)->end()
+                    ->end()
+                ->end()
+                ->arrayNode('scheduling')
+                    ->prototype('integer')->end()
+                ->end()
+                ->arrayNode('notifications')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('subject')->isRequired()->end()
+                            ->scalarNode('description')->isRequired()->end()
+                            ->integerNode('schedule')->isRequired()->end()
+                            ->scalarNode('template')->isRequired()->end()
+                            ->booleanNode('enabled')->defaultValue(true)->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
