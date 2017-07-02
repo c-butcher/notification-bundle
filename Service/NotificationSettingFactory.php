3cccba93373944361793c6d0ffe898c073bf16f4
Chris Butcher <c.butcher@hotmail.com>
2017-07-02 10:06:04 -0700

    Changed create method to return the setting if it already exists. Added comments.

--- Service/NotificationSettingFactory.php
+++ Service/NotificationSettingFactory.php
@@ -5,16 +5,40 @@ namespace KungFu\NotificationBundle\Service;
 use Doctrine\ORM\EntityManagerInterface;
 use KungFu\NotificationBundle\Entity\NotificationSettingInterface;
 
+/**
+ * Class NotificationSettingFactory
+ *
+ * @package KungFu\NotificationBundle\Service
+ * @author Chris Butcher <c.butcher@hotmail.com>
+ */
 class NotificationSettingFactory implements NotificationSettingFactoryInterface
 {
+    /**
+     * @var string
+     */
     protected $class;
 
+    /**
+     * @var array
+     */
     protected $config;
 
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
     protected $repo;
 
+    /**
+     * @var EntityManagerInterface
+     */
     protected $manager;
 
+    /**
+     * NotificationSettingFactory constructor.
+     *
+     * @param EntityManagerInterface $manager
+     * @param array $config
+     */
     public function __construct(EntityManagerInterface $manager, array $config)
     {
         $this->config  = $config;
@@ -23,6 +47,13 @@ class NotificationSettingFactory implements NotificationSettingFactoryInterface
         $this->repo    = $manager->getRepository($this->class);
     }
 
+    /**
+     * Returns all of the notification settings for a specific user.
+     *
+     * @param integer $userId
+     *
+     * @return array
+     */
     public function getAllByUser($userId)
     {
         return $this->repo->findBy(array(
@@ -30,6 +61,14 @@ class NotificationSettingFactory implements NotificationSettingFactoryInterface
         ));
     }
 
+    /**
+     * Returns a specific notification setting for a specific user.
+     *
+     * @param integer $userId
+     * @param string $key
+     *
+     * @return null|object
+     */
     public function getByUserKey($userId, $key)
     {
         return $this->repo->findOneBy(array(
@@ -38,11 +77,24 @@ class NotificationSettingFactory implements NotificationSettingFactoryInterface
         ));
     }
 
+    /**
+     * Creates a users notification setting.
+     *
+     * @param integer $userId
+     * @param string $key
+     *
+     * @return null|NotificationSettingInterface
+     *
+     * @throws \Exception
+     */
     public function create($userId, $key)
     {
-        if (($setting = $this->getByUserKey($userId, $key)) === null) {
-            $setting = new $this->class();
+        /** @var NotificationSettingInterface $setting */
+        if (($setting = $this->getByUserKey($userId, $key)) !== null) {
+            return $setting;
         }
+
+        $setting = new $this->class();
 
         if (!($setting instanceof NotificationSettingInterface)) {
             throw new \Exception("The notification settings class must implement the NotificationSettingInterface.");
@@ -62,6 +114,15 @@ class NotificationSettingFactory implements NotificationSettingFactoryInterface
         return $setting;
     }
 
+    /**
+     * Updates a users notification setting.
+     *
+     * @param int $userId
+     * @param string $key
+     * @param bool $enabled
+     *
+     * @return NotificationSettingInterface|null|object
+     */
     public function update($userId, $key, $enabled)
     {
         if (($setting = $this->getByUserKey($userId, $key)) === null) {
@@ -71,5 +132,7 @@ class NotificationSettingFactory implements NotificationSettingFactoryInterface
         $setting->setEnabled($enabled);
 
         $this->manager->flush();
+
+        return $setting;
     }
 }
